d21892006089385fbd40f248a9dd8614ce1f48c7
takumi0706 <ganndamu0706@gmail.com>
2024-05-13 13:56:14 +0900

    commit:collect run.py and create data.db

--- 03_table_join/run.py
+++ 03_table_join/run.py
@@ -38,4 +38,107 @@ def make_table(cur, con):
                 user_name TEXT UNIQUE NOT NULL)
                 """)
 
-    
+    users_list = [(0,"Ichiro"),(1,"Jiro"),(2,"Saburo"),(3,"Shiro")]
+    cur.executemany(f"INSERT INTO user_table VALUES(?,?)", users_list)
+    sql_result("SELECT FROM user_tabel", con)
+
+    print("-----------------------------")
+    print("item_table")
+
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS item_table(
+                item_id INTEGER PRIMARY KEY,
+                item_name TEXT UNIQUE NOT NULL,
+                item_price INTEGER CHECK( item_price >= 0),
+                description TEXT DEFAULT 'No description'
+                )
+                """)
+
+    items_list = [(0,"apple",100,"Delicious"),
+                  (1,"banana",80,"Sweet"),
+                  (2,"orange",120,"Juicy")]
+
+    for id, name, price, description in items_list:
+        if not description:
+            cur.execute(f"""
+                        INSERT INTO item_table(item_id, item_name, item_price)
+                        VALUES({id},'{name}',{price})
+                        """)
+        else:
+            cur.execute(f"""
+                        INSERT INTO item_table(item_id, item_name, item_price, description)
+                        VALUES ({id},'{name}',{price},'{description}')
+                        """)
+        sql_result("SELECT * FROM item_table", con)
+
+    print("-----------------------------")
+    print("buy_table")
+
+    cur.execute("""
+                CREATE TABEL buy_table(
+                buy_id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                item_id INTEGER,
+                Created_time TEXT NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES user_table(user_id),
+                FOREIGN KEY(item_id) REFERENCES item_table(item_id)
+                )
+                """)
+
+    buy_list = [(0,0,1,str(datetime.datetime.now())),
+                (1,1,2,str(datetime.datetime.now())),
+                (2,1,1,str(datetime.datetime.now())),
+                (3,5,1,str(datetime.datetime.now()))]
+
+    cur.executemay(f"INSERT INTO buy_table VALUES (?,?,?,?)", buy_list)
+    sql_result("SELECT * FROM buy_table", con)
+
+def join_table(con):
+    print("-----------------------------")
+    print("(INNER) JOIN table")
+
+    sql_result = """
+                SELECT *
+                FROM buy_table
+                JOIN user_table
+                ON buy_table.user_id = user_table.user_id
+                """
+
+    sql_result(sql_text, con)
+
+    print("-----------------------------")
+    print("LEFT (OUTER) JOIN table")
+
+    sql_text = """
+    SELECT *
+    FROM buy_table
+    LEFT JOIN user_table
+    ON buy_table.user_id = user_table.user_id
+    """
+
+    sql_result(sql_text, con)
+
+    print("-----------------------------")
+    print("RIGHT (OUTER) JOIN table")
+
+    sql_text = """
+    SELECT * 
+    FROM buy_table
+    RIGHT JOIN user_table
+    ON buy_table.user_id = user_table.user_id
+    """
+    sql_result(sql_text, con)
+
+    print("-----------------------------")
+    print("FULL (OUTER) JOIN table")
+
+    sql_text = """
+    SELECT *
+    FROM buy_table
+    FULL JOIN user_table
+    ON buy_table.user_id = user_table.user_id
+    """
+    sql_result(sql_text, con)
+
+if __name__ == "__main__":
+    main()
